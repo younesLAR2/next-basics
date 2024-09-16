db9c1ec1d9305f1b5802f93467f742861caff699
YounesLAR2 <younes.larfa2@gmail.com>
2024-09-16 18:13:01 +0100

    add a server action that stores files inside a tmp folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
-
+import { promises as fs} from "fs"
 export default function Home() {
 
   async function action(formData: FormData){
     "use server"
     const file = formData.get("file") as File 
-    console.log(file)
+    if(!file || file.size === 0)  {
+      console.log("no file uploaded")
+      return { error: "No File Uploaded"}
+    }
+    const data = await file.arrayBuffer();
+    await fs.writeFile(`${process.cwd()}/tmp/${file.name}`,Buffer.from(data))
   }
   return (
     <div className="min-h-[100vh] flex justify-center items-center p-5 bg-gray-500">

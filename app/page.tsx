5ea8b78bf2f55d03e540345a6b2a28d6e806909e
YounesLAR2 <younes.larfa2@gmail.com>
2024-09-16 12:02:20 +0100

    add a server action that handles file in the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  async function action(formData: FormData){
+    "use server"
+    const file = formData.get("file") as File 
+    console.log(file)
+  }
   return (
-    <main>
-      
-    </main>
+    <div className="min-h-[100vh] flex justify-center items-center p-5 bg-gray-500">
+      <form action={action} className="flex flex-col gap-4 bg-white p-5 rounded-full">
+        <input type="file" name="file" accept=".pdf" />
+        <button type="submit" className="py-4 px-2 bg-black text-white rounded">Upload</button>
+      </form>
+    </div>
   );
 }
